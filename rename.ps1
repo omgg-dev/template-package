664205b6eba8e9ad9985ec624d5945010c33575c
PharaEthan <ethan1.hernou@epitech.eu>
2025-08-15 11:22:07 +0200

    build(package-builder): adds jobs to the build workflow
    These jobs are 'checkLicense' that check the validity of the Unity license use in this repository and the second one 'exportPackage' build the Unity package on all platform given (per default only windows 64 is given) and create a artifact store in the result of the action run.
    This commit also add a 'rename.ps1' script that rename everything in the repository with the new project name given, this script will be use when a new project will be created with this template.

---
+++ rename.ps1
@@ -0,0 +1,69 @@
+# Script that renames a OMGG Template Package into a new name
+
+param(
+    [string]$NewName
+)
+
+$OldName    = "template-package"
+$RootDir    = Get-Location
+$ProjectDir = "$RootDir\$OldName"
+
+if (-not $NewName) {
+    Write-Host "Use: .\rename.ps1 NewName"
+    exit
+} if (-not (Test-Path $ProjectDir)) {
+    Write-Host "Folder $OldName not found."
+    exit
+}
+
+Write-Host "Renaming Unity project to: $NewName"
+
+# 1. Renaming root folder
+Rename-Item $ProjectDir "$NewName"
+
+# 2. Modifying ProjectSettings
+$SettingsFile = "$RootDir\$NewName\ProjectSettings\ProjectSettings.asset"
+
+(Get-Content $SettingsFile) -replace $OldName, $NewName | Set-Content $SettingsFile
+
+# 3. Renaming PACKAGE_NAME and .meta
+$PackageDir = "$RootDir\$NewName\Assets\OMGG\Package"
+
+if (Test-Path "$PackageDir\PACKAGE_NAME") {
+    Rename-Item "$PackageDir\PACKAGE_NAME" $NewName
+
+    if (Test-Path "$PackageDir\PACKAGE_NAME.meta") {
+        Rename-Item "$PackageDir\PACKAGE_NAME.meta" "$NewName.meta"
+    }
+}
+
+# 4. Renaming .asmdef and .asmdef.meta
+$AsmdefFile = "$PackageDir\$NewName\OMGG.PACKAGE_NAME.asmdef"
+
+if (Test-Path $AsmdefFile) {
+    Rename-Item $AsmdefFile "OMGG.$NewName.asmdef"
+
+    if (Test-Path "$AsmdefFile.meta") {
+        Rename-Item "$AsmdefFile.meta" "OMGG.$NewName.asmdef.meta"
+    }
+
+    (Get-Content "$PackageDir\$NewName\OMGG.$NewName.asmdef") -replace "OMGG\.Package\.PACKAGE_NAME", "OMGG.Package.$NewName" | Set-Content "$PackageDir\$NewName\OMGG.$NewName.asmdef"
+}
+
+# 5. Renaming $OldName to $NewName variables in .github/workflows
+
+$WorkflowsDir = "$RootDir\.github\workflows"
+
+if (Test-Path $WorkflowsDir) {
+    Get-ChildItem $WorkflowsDir -Recurse -File | ForEach-Object {
+        (Get-Content $_.FullName) -replace $OldName, $NewName | Set-Content $_.FullName
+    }
+}
+
+# 6. Renaming $OldName to $NewName in rename.ps1 script
+$RenameFile = "$RootDir\rename.ps1"
+
+(Get-Content $RenameFile) -replace $OldName, $NewName | Set-Content $RenameFile
+(Get-Content $RenameFile) -replace "PACKAGE_NAME", $NewName | Set-Content $RenameFile
+
+Write-Host "Project successfully renamed to $NewName"
